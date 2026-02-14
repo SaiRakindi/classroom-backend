65bb293d511c101c3bbe9d3a0b2a947815e4fcaf
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-14 12:23:20 +0530

    feat: cors implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
