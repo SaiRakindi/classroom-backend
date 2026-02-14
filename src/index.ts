bc7914fef5083116a1f84cd8dcdefca0fea9345b
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-14 11:43:55 +0530

    feat: subjects get api implementaion

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
+import subjectsRouter from "./routes/subjects";
+
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to the classroom API");
