3dfe8406e5a347add00063c972fd64236ee055af
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-22 17:16:48 +0530

    feat: file extensions

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -9,7 +9,7 @@ import {
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
-import { users } from "./auth";
+import { users } from "./auth.js";
 
 const timestamps = {
   createdAt: timestamp("created_at").defaultNow().notNull(),
